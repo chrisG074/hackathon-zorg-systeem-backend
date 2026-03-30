7caafa02b8786cb66983114357d0bdcf40f71219
christian G <0355639@student.rocvantwente.nl>
2026-03-30 14:35:00 +0200

    added packages for database and updated program.cs for roles

---
+++ SoftZorg/SoftZorg/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SoftZorg.Data
+{
+    // IdentityDbContext verzorgt de tabellen voor Users, Roles en UserRoles
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
